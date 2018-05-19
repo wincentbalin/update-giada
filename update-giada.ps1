f7d3aa260efc11f02ea85badad6f3bce79171bed
Wincent Balin <wincent.balin@gmail.com>
2018-05-19 12:34:43 +0200

    Updated links, as website structure changed

--- update-giada.ps1
+++ update-giada.ps1
@@ -1,10 +1,10 @@
 ﻿# Update Giada loop machine
 
 $giadaDirectory = "C:\Tools\Giada"
-$giadaUri = "https://www.giadamusic.com/"
+$giadaUri = "https://www.giadamusic.com/download"
 
 # Get version of the remote Giada
-if ((Invoke-WebRequest -Uri $giadaUri).Content -notmatch ".*latest version: ([\d\.]+) .*")
+if ((Invoke-WebRequest -Uri $giadaUri).Content -notmatch ".*Changes in version ([\d\.]+).*")
 {
     Write-Output "Could not find current version at $giadaUri"
     exit 1
@@ -24,7 +24,7 @@ else
     Write-Output "Updating..."
     # Download archive
     $tmpZipFile = "$env:TEMP\giada.zip"
-    $giadaArchiveUri = "https://www.giadamusic.com/download/grab/windows-64"
+    $giadaArchiveUri = "https://www.giadamusic.com/data/giada-$giadaRemoteVersion-win-amd64.zip"
     Invoke-WebRequest -Uri $giadaArchiveUri -OutFile $tmpZipFile
     # Expand archive
     Expand-Archive -LiteralPath $tmpZipFile -DestinationPath $giadaDirectory -Force
